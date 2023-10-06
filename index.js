f6772c343dbb4707e546e65528e5a3a4e0b69346
rafael <rafaelduartesfns@gmail.com>
2023-10-05 23:16:06 -0300

    *

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+var cadastro = document.getElementById('Cadastro')
+
+cadastro.addEventListener('submit', function(event){
+    event.preventDefault()
+
+    var email = document.getElementById('usuario').value
+    var username = document.getElementById('email').value
+    var password = document.getElementById('senha').value
+    var confirm_password = document.getElementById('confirm-senha').value
+
+
+    console.log(username)
+    console.log(email)
+    console.log(password)
+    console.log(confirm_password)
+})
+
+
+
+
+
+
+
+
 const sqlite3 = require('sqlite3').verbose();
 
 const db = new sqlite3.Database('user.db', sqlite3.OPEN_READWRITE, (err)=>{
@@ -5,12 +29,6 @@ const db = new sqlite3.Database('user.db', sqlite3.OPEN_READWRITE, (err)=>{
     
     console.log("connection successful");
 });
-
-
-//db.run('CREATE TABLE users(username, email, password)')
-
-
-
 
 db.close((err) => {
     if (err) return console.error(err.message);
